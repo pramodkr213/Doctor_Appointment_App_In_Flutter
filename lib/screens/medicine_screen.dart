92476d9a265aeb69fb568125c35fd1cffb19d64c
pramodkr213 <144945929+pramodkr213@users.noreply.github.com>
2025-01-10 13:59:51 +0530

    Update medicine_screen.dart
    ///Sry...Sir, I have changed this...I don't know too many of Git Hub's rules. So, Please pardon me...
    debugShowCheckedModeBanner: false

--- lib/screens/medicine_screen.dart
+++ lib/screens/medicine_screen.dart
@@ -95,6 +95,8 @@ class MedicineScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      ///Sry...Sir, I have changed this...I don't know too many of Git Hub's rules. So, Please pardon me...
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Medicine List'),
