2f29d3750d1e87520ec605fb32501d979a80aec9
pramodkr213 <144945929+pramodkr213@users.noreply.github.com>
2025-01-10 13:54:21 +0530

    Update ambulance_screen.dart
    ///Sry...Sir, I have changed this...I don't know too many of Git Hub's rules. So, Please pardon me...
    debugShowCheckedModeBanner: false,

--- lib/screens/ambulance_screen.dart
+++ lib/screens/ambulance_screen.dart
@@ -89,6 +89,8 @@ class AmbulanceScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+         ///Sry...Sir, I have changed this...I don't know too many of Git Hub's rules. So, Please pardon me...
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Ambulance Service'),
